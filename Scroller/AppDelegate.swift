580f74d86d8013f1d5c68c9f90f3f4d642018d6c
Ari Fiorino <arifiorino@gmail.com>
2024-06-13 15:22:15 -0500

    Refactoring

--- Scroller/AppDelegate.swift
+++ Scroller/AppDelegate.swift
@@ -9,66 +9,62 @@ import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
-    private var monitor2: Any?
-
-    func applicationDidFinishLaunching(_ notification: Notification) {
-        let key: String = kAXTrustedCheckOptionPrompt.takeUnretainedValue() as String
-        let options = [key: true]
-        let enabled = AXIsProcessTrustedWithOptions(options as CFDictionary)
-        if !enabled {
-            NSApp.terminate(nil)
-        }
-
-        var monitor: Any?
-        var lastMousePosition: CGPoint?
-        var initialPos: CGPoint?
-        var lastDelta: CGPoint?
-        self.monitor2 = NSEvent.addGlobalMonitorForEvents(matching: .flagsChanged) { event in
-            if let monitor3 = monitor {
-                NSEvent.removeMonitor(monitor3)
-            }
-            monitor = nil
-            if event.modifierFlags.contains(.option){
-                if initialPos == nil{
-                    initialPos = CGEvent(source: nil)!.location
-                }
-                monitor = NSEvent.addGlobalMonitorForEvents(matching: .mouseMoved) { _ in
-                    let point = CGEvent(source: nil)!.location
-                    
-                    if let lastPosition = lastMousePosition {
-                        let mouseDelta = CGPoint(x: lastPosition.x - point.x, y: lastPosition.y - point.y)
-                        if mouseDelta == CGPoint(){
-                            return
-                        }
-                        var scroll = true
-                        if let lastDelta2 = lastDelta{
-                            if mouseDelta == CGPoint(x:-lastDelta2.x,y:-lastDelta2.y){
-                                scroll = false
-                            }
-                        }
-                        lastDelta=mouseDelta
-                        if scroll{
-                            let event = CGEvent(scrollWheelEvent2Source: nil, units: .pixel, wheelCount: 2, wheel1: Int32(-mouseDelta.y), wheel2: Int32(-mouseDelta.x), wheel3: 0)
-                            event?.post(tap: CGEventTapLocation.cghidEventTap)
-                        }
-                        let event2 = CGEvent(mouseEventSource: nil, mouseType: CGEventType.mouseMoved, mouseCursorPosition: initialPos!, mouseButton: CGMouseButton.left)
-                        event2?.post(tap: CGEventTapLocation.cgSessionEventTap)
-                    }
-                    lastMousePosition = point
-                }
-            }else{
-                lastMousePosition = nil
-                initialPos = nil
-            }
-        }
+  private var flagsMonitor: Any?
+  let SCROLL_SPEED = 1.0; // Change to be any speed you want!
+  let SCROLL_FLAG = NSEvent.ModifierFlags.option; // Change to be any key you want!
+  
+  func applicationDidFinishLaunching(_ notification: Notification) {
+    let key: String = kAXTrustedCheckOptionPrompt.takeUnretainedValue() as String
+    let options = [key: true]
+    let enabled = AXIsProcessTrustedWithOptions(options as CFDictionary)
+    if !enabled {
+      NSApp.terminate(nil)
     }
-
-    func applicationWillTerminate(_ notification: Notification) {
-        if let monitor2 = monitor2 {
-            NSEvent.removeMonitor(monitor2)
+    
+    var mouseMonitor: Any?
+    var initialPos: CGPoint?
+    var lastPos: CGPoint?
+    var lastDelta: CGPoint?
+    flagsMonitor = NSEvent.addGlobalMonitorForEvents(matching: .flagsChanged) { event in
+      if let mouseMonitor = mouseMonitor {
+        NSEvent.removeMonitor(mouseMonitor)
+      }
+      mouseMonitor = nil
+      if event.modifierFlags.contains(self.SCROLL_FLAG){
+        initialPos = CGEvent(source: nil)!.location
+        mouseMonitor = NSEvent.addGlobalMonitorForEvents(matching: .mouseMoved) { _ in
+          let pos = CGEvent(source: nil)!.location
+          if let lastPos = lastPos {
+            let delta = CGPoint(x: lastPos.x - pos.x, y: lastPos.y - pos.y)
+            if delta == CGPoint(){
+              return
+            }
+            var scroll = true
+            if let lastDelta = lastDelta{
+              if delta == CGPoint(x:-lastDelta.x,y:-lastDelta.y){
+                scroll = false
+              }
+            }
+            if scroll{
+              let scrollEvent = CGEvent(scrollWheelEvent2Source: nil, units: .pixel, wheelCount: 2, wheel1: Int32(-delta.y * self.SCROLL_SPEED), wheel2: Int32(-delta.x * self.SCROLL_SPEED), wheel3: 0)
+              scrollEvent?.post(tap: CGEventTapLocation.cghidEventTap)
+            }
+            let mouseEvent = CGEvent(mouseEventSource: nil, mouseType: CGEventType.mouseMoved, mouseCursorPosition: initialPos!, mouseButton: CGMouseButton.left)
+            mouseEvent?.post(tap: CGEventTapLocation.cghidEventTap)
+            lastDelta=delta
+          }
+          lastPos = pos
         }
+      }else{
+        initialPos = nil
+        lastPos = nil
+      }
     }
-
-
+  }
+  
+  func applicationWillTerminate(_ notification: Notification) {
+    if let flagsMonitor = flagsMonitor {
+      NSEvent.removeMonitor(flagsMonitor)
+    }
+  }
 }
-
